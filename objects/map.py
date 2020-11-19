315b4eea029cb1424f857fc4ecb736cf4c010346
Литвиненко Максим <ltv.maxym@gmail.com>
2020-11-19 18:12:44 +0300

    Added advanced map renderer. Added sprite loaded

--- objects/map.py
+++ objects/map.py
@@ -5,18 +5,18 @@ class Map:
     map_path = 'pacman.map'
 
     @staticmethod
-    def extract(map_path):
+    def extract(map_path, screen):
         tile_arr = []
         with open(map_path, 'r') as mp:
             tile_code_arr = [list(i.strip()) for i in mp.readlines()]
             for row in range(len(tile_code_arr)):
-                tile_arr.extend([Tile(col, row, tile_code_arr[row][col]) for col in range(len(tile_code_arr[row]))])
+                tile_arr.extend([Tile(col, row, tile_code_arr[row][col], screen) for col in range(len(tile_code_arr[row]))])
 
         return tile_arr
 
     def __init__(self, game):
         self.game = game
-        self.tile_arr = self.extract(self.map_path)
+        self.tile_arr = self.extract(self.map_path, self.game.screen)
 
     def process_draw(self):
         for i in range(len(self.tile_arr)):
--- objects/tile.py
+++ objects/tile.py
@@ -3,14 +3,14 @@ from constants import Content
 
 
 class Tile:
-    tilesize = 10
+    tilesize = 50
 
-    def __init__(self, x=0, y=0, cnt=Content.EMPTY, screen=None):
+    def __init__(self, x, y, cnt, screen):
         # Rect object for hitboxing
         self.rect = pygame.rect.Rect(x*self.tilesize, y*self.tilesize, self.tilesize, self.tilesize)
         # real matrix coordinates
-        self.real_x = self.rect.x//self.tilesize
-        self.real_y = self.rect.y//self.tilesize
+        self.real_x = self.rect.x // self.tilesize
+        self.real_y = self.rect.y // self.tilesize
         # what's inside the tile
         self.content = cnt
         self.screen = screen
@@ -20,9 +20,17 @@ class Tile:
         return self.content
 
     def __set_sprite(self):
-        '''sets the sprite basing on content of the tile'''
-        if self.content == Content.WALL:
-            self.sprite = pygame.image.load('sprites/wall.png')
+        """
+        Sprites in .sprites folder must have same names as fields in Content class
+        """
+        try:
+            for attr in dir(Content):
+                v = getattr(Content, attr)
+                if v == self.content:
+                    self.sprite = pygame.image.load(f'sprites/{attr.lower()}.png')
+                    break
+        except:
+            self.sprite = pygame.image.load('sprites/error_sprite.png')
 
     def draw(self):
         '''blits tile sprite on the exact coordinates of pygame window'''
