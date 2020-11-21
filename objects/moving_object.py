d1d46503fb4028ee62acf4c36607935d0d79338a
Zaharov <zaharov.andrey161@gmail.com>
2020-11-21 16:28:11 +0300

    Pacman movement rework.
    Added FPS lock. Pacman movement now linked to matrix tiles.
    Still have a bug with double-move on first button press.

--- objects/moving_object.py
+++ objects/moving_object.py
@@ -4,23 +4,22 @@ from constants import tilesize
 
 
 class MovingObject(DrawableObject):
-    def __init__(self, game, x, y, sprite, content, speed):
+    def __init__(self, game, x, y, sprite, content):
         super().__init__(game)
+        # pygame screen pos
         self.rect.x = x*tilesize
         self.rect.y = y*tilesize
         self.rect.w, self.rect.h = tilesize, tilesize
-        self.speed = speed
-        self.shift_y = 0
-        self.shift_x = 0
-
-        self.matrix_x = x
-        self.matrix_y = y
-
+        # real matrix position
+        self.real_x = x
+        self.real_y = y
+        # appearence
         self.sprite = sprite
         self.content = content
 
-    def move(self):
-        pass
+    def update_pos(self):
+        self.rect.x = self.real_x * tilesize
+        self.rect.y = self.real_y * tilesize
 
     def process_draw(self):
         self.game.screen.blit(self.sprite, self.rect)
--- objects/pacman.py
+++ objects/pacman.py
@@ -6,37 +6,20 @@ class Pacman(MovingObject):
     pacman_sprite = 'sprites/pacman.png'
 
     def __init__(self, game, x, y, speed):
-        super().__init__(game, x, y, pygame.image.load(self.pacman_sprite), Content.PACMAN, speed)
+        super().__init__(game, x, y, pygame.image.load(self.pacman_sprite), Content.PACMAN)
 
     def process_move(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                self.shift_x = -1
-            elif event.key == pygame.K_d:
-                self.shift_x = 1
-            if event.key == pygame.K_w:
-                self.shift_y = -1
-            elif event.key == pygame.K_s:
-                self.shift_y = 1
-            # print(self.shift_x, self.shift_y)
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_a or event.key == pygame.K_d:
-                self.shift_x = 0
-            if event.key == pygame.K_w or event.key == pygame.K_s:
-                self.shift_y = 0
-        self.move()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]: self.move(-1, 0)
+        elif keys[pygame.K_d]: self.move(1, 0)
+        elif keys[pygame.K_w]: self.move(0, -1)
+        elif keys[pygame.K_s]: self.move(0, 1)
 
-    def move(self):
-        self.rect.x += self.speed * self.shift_x
-        self.rect.y += self.speed * self.shift_y
-
-    def check_collision(self):
-        # ghost collision check
-        # cherry collision check
-        # dot collision check
-        # power-up collision check
-        # wall collision check
-        pass
+    def move(self, x, y):
+        self.real_x += x
+        self.real_y += y
+        print(self.real_x, self.real_y)
+        self.update_pos()
 
     def process_event(self, event):
         self.process_move(event)
