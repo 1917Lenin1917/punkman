6a8194637404280146c4032ddd80fed3fdb2e1e8
Zaharov Andrey <zaharov.andrey161@gmail.com>
2020-12-11 19:56:21 +0300

    Hotfix. Nothing more.

--- objects/pacman.py
+++ objects/pacman.py
@@ -7,7 +7,10 @@ from objects import Map
 
 class Pacman(MovingObject):
     pacman_sprite = 'sprites/pacman.png'
-
+    # self.pacman_sprite_u = 'sprites/pacman_u.png'
+    # self.pacman_sprite_d = 'sprites/pacman_d.png'
+    # self.pacman_sprite_l = 'sprites/pacman_l.png'
+    # self.pacman_sprite_r = 'sprites/pacman_r.png'
     def __init__(self, game, x, y, map_ref: Map):
         super().__init__(game, x, y, pygame.image.load(self.pacman_sprite), Content.PACMAN, map_ref)
         self.points = 0
@@ -15,6 +18,10 @@ class Pacman(MovingObject):
         self.lives = 3
         self.dir = (0, 0)
         self.cache_dir = (0, 0)
+        # self.sprite_u = pygame.image.load(self.pacman_sprite_u)
+        # self.sprite_d = pygame.image.load(self.pacman_sprite_d)
+        # self.sprite_l = pygame.image.load(self.pacman_sprite_l)
+        # self.sprite_r = pygame.image.load(self.pacman_sprite_r)
 
     def process_event(self, event: pygame.event.Event):
         if event.type == pygame.KEYDOWN:
@@ -26,12 +33,13 @@ class Pacman(MovingObject):
     def process_move(self):
         if self.is_able_to_move(*self.cache_dir):
             self.dir = self.cache_dir
+        
+        if self.is_able_to_move(*self.dir):
+            self.move(*self.dir)
         else:
             self.dir = (0, 0)
-            self.cache_dir = (0, 0)
-
-        if self.is_able_to_move(*self.dir): 
-            self.move(*self.dir)
+            # self._sprite_update()
+        
 
 
     def move(self, x_increment, y_increment):
@@ -43,7 +51,7 @@ class Pacman(MovingObject):
         self.update_pos()
 
     def check_teleportation(self):
-        if self.map.teleport1 is None: return
+        if self.map.teleport1 is None: return 
 
         if self.real_x == self.map.teleport1[0] and self.real_y == self.map.teleport1[1]:
             self.real_x = self.map.teleport2[0]-1
@@ -69,3 +77,15 @@ class Pacman(MovingObject):
     def respawn_logic(self):
         self.dir = (0, 0)
         self.cache_dir = (0, 0)
+    
+    def _sprite_update(self):
+        if self.dir[0] == 1:
+            pass # self.sprite_r
+        elif self.dir[0] == -1:
+            pass # self.sprite_l
+        elif self.dir[1] == 1:
+            pass # self.sprite_d
+        elif self.dir[1] == -1:
+            pass # self.sprite_u
+        elif self.dir == (0, 0):
+            pass # self.sprite
